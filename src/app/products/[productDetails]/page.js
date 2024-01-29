5f77bdee8118eebf0ecca57376787edf94fcd5f4
Ahmad Raza <ahmadraza77887087@gmail.com>
2024-01-30 04:19:04 +0500

    Fetch details of every single product dynamically

--- src/app/products/[productDetails]/page.js
+++ src/app/products/[productDetails]/page.js
@@ -1,9 +1,23 @@
 import React from 'react'
 
-export default function page() {
+async function getProductDetails(id) {
+  // console.log(id)
+  const response = await fetch(`https://dummyjson.com/products/${id}`, {
+    method:'GET'
+  })
+  const result = await response.json();
+  // console.log(result);
+  return result;
+}
+export default async function page({params}) {
+  // console.log(params);
+  const {productDetails} = params;
+  const details = await getProductDetails(productDetails); 
   return (
     <div>
-      <p>This is the product details.</p>
+      <h1> <b>Product Name </b> : {details.title}</h1>
+      <p> <b>Product Category </b>: {details.category}</p>
+      <p> <b>Product Description </b>: {details.description}</p>
     </div>
   )
 }
